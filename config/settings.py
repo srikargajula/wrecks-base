8dc1142a4844a32f35de59f21c7564fb4c301e1f
srikargajula <srikargajula18@gmail.com>
2026-07-18 08:03:48 +0530

    Configure Django for EC2 deployment

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n@^_(k3nidm&*v%-ur4o=plu)&cj^y0)2qgwge*esgip%r*v_c'
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-4j9!9#dH8kL2@vA5^xYp8NfQ7zLmR1cB3wE6tY"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True") == "True"
@@ -28,7 +31,8 @@ DEBUG = os.environ.get("DEBUG", "True") == "True"
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
-    ".onrender.com",
+    "52.63.222.106",
+    "ec2-52-63-222-106.ap-southeast-2.compute.amazonaws.com",
 ]
 
 
@@ -137,18 +141,18 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
-import cloudinary
-
 CLOUDINARY_STORAGE = {
-    "CLOUD_NAME": "x84rtezm",
-    "API_KEY": "617944871518177",
-    "API_SECRET": "7bSVCtdZJvxnxE-ReFO7hFoctWk",
+    "CLOUD_NAME": os.environ.get("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": os.environ.get("CLOUDINARY_API_KEY"),
+    "API_SECRET": os.environ.get("CLOUDINARY_API_SECRET"),
 }
 
+import cloudinary
+
 cloudinary.config(
-    cloud_name=CLOUDINARY_STORAGE["CLOUD_NAME"],
-    api_key=CLOUDINARY_STORAGE["API_KEY"],
-    api_secret=CLOUDINARY_STORAGE["API_SECRET"],
+    cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.environ.get("CLOUDINARY_API_KEY"),
+    api_secret=os.environ.get("CLOUDINARY_API_SECRET"),
     secure=True,
 )
 
